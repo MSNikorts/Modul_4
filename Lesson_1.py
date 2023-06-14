35c39cd32f692566b091d74f6dfc371cb6ac7215
Maxim <strokin.maxim2007@gmail.com>
2023-06-14 19:40:16 +0300

    initial

---
+++ Lesson_1.py
@@ -0,0 +1,9 @@
+def strcounter(s):
+    for sym in s:
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+        print(sym, counter)
+
+strcounter('aaaafgjihgaadfgh')
